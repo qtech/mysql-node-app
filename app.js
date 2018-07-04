adb3444113ec9b503575a89be59746991100de10
AlphaQtech <viral.tech1211@gmail.com>
2018-07-04 12:09:19 +0530

    Basic SQL promise created and used

--- app.js
+++ app.js
@@ -1,7 +1,14 @@
 const express = require('express');
 const app = express();
 const mysql = require('mysql');
+const Q = require('q');
 
+//Everything related to Express
+//Set view Engine 
+app.set('view engine','ejs');
+
+
+//Everything related to Mysql or Database
 //Set database credentials.
 const conn = mysql.createConnection({
     host:'localhost',
@@ -16,13 +23,34 @@ conn.connect((err) => {
     console.log('Database Connected');
 });
 
-//Set view Engine
-app.set('view engine','ejs');
 
-app.get('/',(req,res) => {
-    res.render('index');
+//Everything Promises
+let deferred = Q.defer();
+
+function getUsers(){
+    conn.query('SELECT * FROM users LIMIT 1',deferred.makeNodeResolver());        
+    return deferred.promise;
+}
+
+function getCategories(){
+    conn.query('SELECT * FROM categories LIMIT 1',deferred.makeNodeResolver());
+    return deferred.promise;
+}
+
+//Application Routing
+app.get('/',(req,res) => {                 
+    Q.all([getUsers(),getCategories()])
+    .then((results) => {
+        console.log(results);
+        res.end('Finished Query');
+        // res.render('index',{data:results});
+    })
+    .catch((err) => {
+        res.render('error',{data:err});
+    });        
 });
 
+//Starting the server on port 4000
 app.listen(4000,(err) => {
     if(err) throw err;    
 });
